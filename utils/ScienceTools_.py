22b15f8ce7f1818423dc04da117f61252db3a7c2
Michela Negro <michela.negro@to.infn.it>
2019-03-08 18:50:54 +0100

    should be fine

--- utils/ScienceTools_.py
+++ utils/ScienceTools_.py
@@ -200,6 +200,24 @@ def gtpsf(gtpsf_dict):
                   %(expcube, outfile, irfs, evtype, ra, dec, emin, emax, \
                         nenergies, thetamax, ntheta))
 
+def gtbindef(ebinning_array, file_name='ebinning.txt'):
+    """Produces a fits file defining the enrgy binning to fed gtbin.
+
+           ebinning_array: numpy array
+               array in which the energy binnin is defined.
+    """
+    txt_file_name = os.path.join(GD_OUT, file_name)
+    txt_file = open(txt_file_name, 'w')
+    fits_file_name = os.path.join(GD_OUT, 
+                                  file_name.replace('.txt', '.fits'))
+    for emin, emax in zip(ebinning_array[:-1], ebinning_array[1:]):
+        txt_file.write('%.4f %.4f\n'%(emin, emax))
+    txt_file.close()
+    os.system('gtbindef bintype=E binfile=%s outfile=%s energyunits=MeV' \
+                  %(txt_file_name, fits_file_name))
+    logger.info('Created %s...'%fits_file_name)
+    return fits_file_name
+
 def mergeft1(path_to_files, out_file_name, N1week, Nnweek):
     """creates a .txt file with the list of the FT files to merge.
 
@@ -214,21 +232,54 @@ def mergeft1(path_to_files, out_file_name, N1week, Nnweek):
     """
     if N1week < 9:
         abort('Invalid number of weeks: the minimun must be > or = to 9')
-    if Nnweek > 486:
-        abort('Invalid number of weeks: the maximum must be < or = to 486')
+    if Nnweek > 561:
+        abort('Invalid number of weeks: the maximum must be < or = to 561')
     outtxtfile = os.path.join(path_to_files, out_file_name)
     if not os.path.exists(outtxtfile):
         out_file = open(outtxtfile, 'w')
         for i in range(N1week, Nnweek+1):
             if i == 9:
-                out_file.write("%s/lat_photon_weekly_w00%i_p302_v001.fits \n" \
+                out_file.write("%s/lat_photon_weekly_w00%i_p305_v001.fits \n" \
                                    %(path_to_files,i))
             if i >= 10 and i <= 99:
-                out_file.write("%s/lat_photon_weekly_w0%i_p302_v001.fits \n" \
+                out_file.write("%s/lat_photon_weekly_w0%i_p305_v001.fits \n" \
                                    %(path_to_files,i))
             if i > 99:
-                out_file.write("%s/lat_photon_weekly_w%i_p302_v001.fits \n" \
+                out_file.write("%s/lat_photon_weekly_w%i_p305_v001.fits \n" \
                                    %(path_to_files,i))
+        out_file.close()
+    logger.info('Created %s...' %outtxtfile)
+    return '@'+outtxtfile
+
+def mergeft2(path_to_files, out_file_name, N1week, Nnweek):
+    """creates a .txt file with the list of the FT files to merge.
+
+       path_to_files: str
+           path where datat files are stored
+       out_file_name: str
+           name of the txt output file (created in the same folder of data)
+       N1week: int
+           number of the starting week
+       Nnweek: int
+           number of the ending week
+    """
+    if N1week < 9:
+        abort('Invalid number of weeks: the minimun must be > or = to 9')
+    if Nnweek > 561:
+        abort('Invalid number of weeks: the maximum must be < or = to 561')
+    outtxtfile = os.path.join(path_to_files, out_file_name)
+    if not os.path.exists(outtxtfile):
+        out_file = open(outtxtfile, 'w')
+        for i in range(N1week, Nnweek+1):
+            if i == 9:
+                out_file.write("%s/lat_spacecraft_weekly_w00"+
+                               "%i_p302_v001.fits \n" %(path_to_files,i))
+            if i >= 10 and i <= 99:
+                out_file.write("%s/lat_spacecraft_weekly_w0"+
+                               "%i_p302_v001.fits \n" %(path_to_files,i))
+            if i > 99:
+                out_file.write("%s/lat_spacecraft_weekly_w"+
+                               "%i_p302_v001.fits \n" %(path_to_files,i))
         out_file.close()
     logger.info('Created %s...' %outtxtfile)
     return '@'+outtxtfile
@@ -238,18 +289,17 @@ def main():
     """
     import numpy as np
     from GammaDipole import FT_DATA_FOLDER
-    from GammaDipole.utils.gFTools import ebinning_fits_file
 
     PH = 'photon'
     SC = 'spacecraft'
     PH_FOLDER = os.path.join(FT_DATA_FOLDER, PH)
     SC_FOLDER = os.path.join(FT_DATA_FOLDER, SC)
     FT1_FILE = mergeft1(PH_FOLDER, 'FT1_w9-12.txt', 9, 12)
+    FT2_FILE = mergeft2(PH_FOLDER, 'FT2_w9-12.txt', 9, 12)
     FILTER_CUT='DATA_QUAL==1&&LAT_CONFIG==1&&LAT_MODE==5&&IN_SAA!=T'+\
                '&&((ABS(ROCK_ANGLE)<52))'
     OUT_FILE_LABEL = 'test'
-    FT2_FILE = os.path.join(SC_FOLDER, 'lat_spacecraft_merged.fits')
-    EBINNING_FILE = ebinning_fits_file(np.logspace(3, 3.6, 11))
+    EBINNING_FILE = gtbindef(np.logspace(3, 3.6, 11))
     data_filter = {'infile': FT1_FILE,
                    'emin': 1000,
                    'emax': 5000,
